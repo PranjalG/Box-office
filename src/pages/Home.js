5eeb1c6dbc5d87a8205d8ac8061f009e594e6f20
PranjalG <gaurpranjal236@gmail.com>
2021-12-13 11:40:27 +0530

    added navbar and page components

---
+++ src/pages/Home.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Home() {
+  return <div>This is our home page</div>;
+}
+
+export default Home;
---
+++ src/pages/Starred.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Starred() {
+  return <div>This is our starred page</div>;
+}
+
+export default Starred;
