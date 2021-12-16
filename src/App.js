b7484b428d5c7a5e7ca4704530c1edd1331d0252
PranjalG <gaurpranjal236@gmail.com>
2021-12-16 16:20:06 +0530

    defined theme for styled components, added styles to Title and Navbar

--- src/App.js
+++ src/App.js
@@ -1,27 +1,38 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 
 import Home from './pages/Home';
 import Show from './pages/Show';
 import Starred from './pages/Starred';
 
+const theme = {
+  mainColors: {
+    blue: '#0F52BA',
+    gray: '#c6c6c6',
+    dark: '#353535',
+  },
+};
+
 function App() {
   return (
-    <Switch>
-      <Route exact path="/">
-        <Home />
-      </Route>
+    <ThemeProvider theme={theme}>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
 
-      <Route exact path="/starred">
-        <Starred />
-      </Route>
+        <Route exact path="/starred">
+          <Starred />
+        </Route>
 
-      <Route exact path="/show/:id">
-        <Show> Yo </Show>
-      </Route>
+        <Route exact path="/show/:id">
+          <Show> Yo </Show>
+        </Route>
 
-      <Route>This is 404 page</Route>
-    </Switch>
+        <Route>This is 404 page</Route>
+      </Switch>
+    </ThemeProvider>
   );
 }
 
